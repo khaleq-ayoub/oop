1f590c9d18ceee7fe24491115a0ccc44f025736a
khaleq-ayoub <khaleqayoub509@gmail.com>
2025-09-04 12:31:04 +0200

    Updated code

---
+++ loops day 3/Ex1/main.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int i , N ;
+   printf("Entrer un nombre :\n");
+   scanf(" %d\n", &N);
+   for(int i=10 ;i>=0 ;i--){
+    printf("%d * %d = %d\n", N, i, N*i);
+   }
+    return 0;
+}
