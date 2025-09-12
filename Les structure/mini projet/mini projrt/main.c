ed9121ba032b99de50b7c11b6d90271913f3f945
khaleq-ayoub <khaleqayoub509@gmail.com>
2025-09-12 13:22:40 +0200

    update

--- Les structure/mini projet/mini projrt/main.c
+++ Les structure/mini projet/mini projrt/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 typedef struct{
     char code[60];
     char nom[50];
     int credit;
-    float note[200];
+    float note;
 }cours;
 typedef struct{
     int ID;
@@ -15,11 +15,9 @@ typedef struct{
     float moyenne;
     cours co[10];
 }etudiant ;
-void information(){
-    etudiant e[100];
-    int nb_etudiant=0;
-    int nb_cours=0;
+void information( etudiant e[] , int nb_etudiant , int nb_cours){
 
+    int som=0;
 
 
     for(int i=0 ; i<nb_etudiant ; i++){
@@ -34,17 +32,19 @@ void information(){
         scanf("%d",&e[i].age);
     for(int j=0 ;j<nb_cours ;j++){
         printf("le nom de cour est :");
-        gets(e[i].co[j].nom);
+        scanf("%s",e[i].co[j].nom);
         printf("le code de cours est : ");
-        gets(e[i].co[j].code);
+        scanf("%s",e[i].co[j].code);
         printf("le credit est :");
         scanf("%d",&e[i].co[j].credit);
         printf("la note obtenir dans ce cours est :");
-        scanf("%f",&e[i].co[j].note[100]);
-      e[i].moyenne = e[i].co[j].note[100]/j;
+        scanf("%f",&e[i].co[j].note);
+        som += e[i].co[j].note;
     }
-
+        e[i].moyenne = som / nb_cours;
   }
+
+
 
 
 }
@@ -61,7 +61,8 @@ int main()
     for(int i=0 ;i<nb_etudiant ;i++){
        for(int j=0 ;j<nb_cours ;j++){
     printf("les information de l etudaint \n");
-    printf("les information de l'etudiant %d :ID : %d \tNom complet: %s,%s age= %d ans . Le nom de cours %d est %s son code est %s avec nombre de credit egal a %d , j ais obtenue la note %.2f dans ce cours  ",i+1,e[i].ID,e[i].nom,e[i].prenom,e[i].age,j+1,e[i].co[j].nom,e[i].co[j].code,e[i].co[j].credit,e[i].co[j].note[100]);
+    printf("les information de l'etudiant %d :ID : %d \tNom complet: %s,%s age= %d ans . Le nom de cours %d est %s son code est %s avec nombre de credit egal a %d , j ais obtenue la note %.2f dans ce cours  ",i+1,e[i].ID,e[i].nom,e[i].prenom,e[i].age,j+1,e[i].co[j].nom,e[i].co[j].code,e[i].co[j].credit,e[i].co[j].note);
+    printf("la moyenne des notes est ;%.2f ", e[i].moyenne );
        }
     }
     return 0;
