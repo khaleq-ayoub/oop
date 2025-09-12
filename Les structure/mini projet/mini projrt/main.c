1fa4e42b5ed0b289088df3e1ed7ff7018ded4865
khaleq-ayoub <khaleqayoub509@gmail.com>
2025-09-12 13:02:09 +0200

    update

--- Les structure/mini projet/mini projrt/main.c
+++ Les structure/mini projet/mini projrt/main.c
@@ -13,35 +13,35 @@ typedef struct{
     char prenom[30];
     int age;
     float moyenne;
-    cours co;
+    cours co[10];
 }etudiant ;
 void information(){
     etudiant e[100];
-    int nb_etudiant;
-    int nb_cours;
+    int nb_etudiant=0;
+    int nb_cours=0;
 
 
 
     for(int i=0 ; i<nb_etudiant ; i++){
         printf("les information de l etudiant %d" ,i+1);
         printf("Entrer votre id : ");
-        scanf("%d",e[i].ID);
+        scanf("%d",&e[i].ID);
         printf("Entrer votre nom : ");
         scanf("%s",e[i].nom);
          printf("Entrer votre prenom : ");
         scanf("%s",e[i].prenom);
          printf("Entrer votre age : ");
-        scanf("%d",e[i].age);
+        scanf("%d",&e[i].age);
     for(int j=0 ;j<nb_cours ;j++){
         printf("le nom de cour est :");
-        gets(e[j].co.nom);
+        gets(e[i].co[j].nom);
         printf("le code de cours est : ");
-        gets(e[j].co.code);
+        gets(e[i].co[j].code);
         printf("le credit est :");
-        scanf("%d",e[j].co.credit);
+        scanf("%d",&e[i].co[j].credit);
         printf("la note obtenir dans ce cours est :");
-        scanf("%f",e[j].co.note[j]);
-      e[i].moyenne = e[j].co.note[j]/j;
+        scanf("%f",&e[i].co[j].note[100]);
+      e[i].moyenne = e[i].co[j].note[100]/j;
     }
 
   }
@@ -50,15 +50,19 @@ void information(){
 }
 int main()
 {
+    etudiant e[100];
     int i ;
     int nb_etudiant;
     int nb_cours;
     printf("Enter le nombre d'etudiant : ");
-    scanf("%d",nb_etudiant);
+    scanf("%d",&nb_etudiant);
     printf("Enter le nombre de cours : ");
-    scanf("%d",nb_cours);
-    for(int i=0 ;i<nb)
-    printf("les information de l etudaint ")
-
+    scanf("%d",&nb_cours);
+    for(int i=0 ;i<nb_etudiant ;i++){
+       for(int j=0 ;j<nb_cours ;j++){
+    printf("les information de l etudaint \n");
+    printf("les information de l'etudiant %d :ID : %d \tNom complet: %s,%s age= %d ans . Le nom de cours %d est %s son code est %s avec nombre de credit egal a %d , j ais obtenue la note %.2f dans ce cours  ",i+1,e[i].ID,e[i].nom,e[i].prenom,e[i].age,j+1,e[i].co[j].nom,e[i].co[j].code,e[i].co[j].credit,e[i].co[j].note[100]);
+       }
+    }
     return 0;
 }
